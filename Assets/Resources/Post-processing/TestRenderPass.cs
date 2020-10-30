888f11e4a56aa6291688028d3e6340577935185d
像素魔法师 <pixel-magician@outlook.com>
2020-10-30 14:24:00 +0800

    添加火焰shader（待完善）
    Signed-off-by: 像素魔法师 <pixel-magician@outlook.com>

---
+++ Assets/Resources/Post-processing/TestRenderPass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class TestRenderPass : ScriptableRenderPass
+{
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        
+    }
+}
---
+++ Assets/Resources/Post-processing/TestRendererFeature.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class TestRendererFeature : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class HLSettings
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        public Material mMat;
+        //public Target destination = Target.Color;
+        public int blitMaterialPassIndex = -1;
+        //这是一个shader的propertyId
+        public string textureId = "_ScreenTexture";
+        public float contrast = 0.5f;
+    }
+
+    public HLSettings settings = new HLSettings();
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        
+    }
+
+    public override void Create()
+    {
+        
+    }
+
+}
