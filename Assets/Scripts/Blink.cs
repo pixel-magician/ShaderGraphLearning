3eb3b436a03c4fe90d3e475ffec8e2f6b5080e54
像素魔法师 <pixel-magician@outlook.com>
2020-11-12 15:27:27 +0800

    实现小猪眨眼动画
    Signed-off-by: 像素魔法师 <pixel-magician@outlook.com>

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+    [SerializeField]
+    MeshRenderer _meshRenderer;
+    [SerializeField]
+    Texture _openEye;
+    [SerializeField]
+    Texture _closeEye;
+    [SerializeField]
+    float _openTimer = 2;
+    [SerializeField]
+    float _closeTimer = 0.1f;
+
+    bool _eyeState = true;
+
+    private void OnEnable()
+    {
+        StartCoroutine(IE_StartOpen());
+    }
+
+    IEnumerator IE_StartOpen()
+    {
+        _meshRenderer.material.SetTexture("TextEye", _openEye);
+        yield return new WaitForSeconds(_openTimer);
+        StartCoroutine(IE_StartClose());
+    }
+
+    IEnumerator IE_StartClose()
+    {
+        _meshRenderer.material.SetTexture("TextEye", _closeEye);
+        yield return new WaitForSeconds(_closeTimer);
+        StartCoroutine(IE_StartOpen());
+    }
+
+}
